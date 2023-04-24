45d1e78ae9ee69c597be6b9c205366a897f40228
SAI <37893685+SaiRohitSingampalli@users.noreply.github.com>
2023-04-23 22:33:58 -0400

    update index.js

--- warehousereceipt-DAPP/warehousereceiptapp/src/index.js
+++ warehousereceipt-DAPP/warehousereceiptapp/src/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var app = express();
 
+app.set('views', './src/views');
+app.use(express.json());
 app.use(express.static('src'));
-app.use(express.static('../../build/contracts'));
+app.use(express.static('../warehousereceiptcontract/build/contracts'));
+
 
 app.get('/', function (req, res) {
-  res.render('index.html');
+  res.redirect('/marketplace');
 });
 
+app.get('/marketplace', function (req, res) {
+  res.render('marketplace.html');
+});
+
+app.get('/myprofile', function (req, res) {
+  res.render('myprofile.html');
+});
+
+app.get('/moderator', function (req, res) {
+  res.render('moderator.html');
+})
+
 app.listen(3000, function () {
-  console.log('Warehouse Receipt Dapp listening on port 3000!');
+  console.log('Dapp listening on port 3000!');
 });
